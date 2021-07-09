6371cf8bf9ac934eef4f075facb3ca5b32d901ff
Muhammad Ghifari <ghifari9567@gmail.com>
2021-07-09 18:33:10 +0700

    Final

--- app/src/main/java/com/example/quran/ui/MainActivity.kt
+++ app/src/main/java/com/example/quran/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.quran.ui
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quran.R
 import com.example.quran.databinding.ActivityMainBinding
+import com.example.quran.ui.adapter.FragmentPagerAdapter
 import com.example.quran.utils.viewBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.pager.apply {
+            val pages = arrayListOf<Int>().apply {
+                for (i in 1..604) {
+                    add(i)
+                }
+            }.reversed().toCollection(arrayListOf())
+            val fragments = pages.map { page->
+                QuranPagedFragment(page)
+            }
+            adapter = FragmentPagerAdapter(this@MainActivity, fragments.toCollection(arrayListOf()))
+            setCurrentItem(pages.indexOf(293), false)
+        }
 
     }
 }
